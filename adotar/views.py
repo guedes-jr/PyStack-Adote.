b3e3aacf531885a000b3be1d553cbb32b99afd81
João Guedes <joao.guedes.developer@gmail.com>
2023-02-19 02:08:50 -0300

    Projeto Finalizado

--- adotar/views.py
+++ adotar/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.messages import constants
 from divulgar.models import Pet, Raca
+from django.http import HttpResponse
+from django.contrib import messages
+from .models import PedidoAdocao
+from datetime import datetime
+from django.core.mail import send_mail
 
+@login_required
 def listar_pets(request):
     if request.method == "GET":
         pets = Pet.objects.filter(status='P')
@@ -22,3 +30,43 @@ def listar_pets(request):
                                                     'cidade': cidade,
                                                     'raca_filter': raca_filter
                                                   })
+@login_required
+def pedido_adocao(request, id_pet):
+    pet = Pet.objects.filter(id=id_pet, status='P')
+    if not pet.exists:
+        messages.add_message(request, constants.WARNING, 'Esse pet já foi adotado!')
+        return redirect('/adotar')
+
+    pedido = PedidoAdocao(pet=pet.first(),
+                          usuario=request.user,
+                          data=datetime.now())
+    pedido.save()
+    messages.add_message(request, constants.SUCCESS, 'Pedido de adição realizado com sucesso!')
+
+    return redirect('/adotar')
+
+@login_required
+def processa_pedido_adocao(request, id_pedido):
+    status = request.GET.get('status')
+    pedido = PedidoAdocao.objects.get(id=id_pedido)
+
+    if status == 'A':
+        pedido.status = 'AP'
+        string = '''olá! Sua adoção foi aprovada com sucesso!....'''
+    elif status == 'R':
+        pedido.status = 'R'
+        string = '''olá! Sua adoção foi Recusada!'''
+
+    pedido.save()
+
+    email = send_mail(
+        'Sua adoção foi processada',
+        string,
+        'meuemail@.gmail.com',
+        [
+            pedido.usuario.email,
+        ]
+    )
+
+    messages.add_message(request, constants.SUCCESS, 'Pedido de adoção processado com sucesso!')
+    return redirect('/divulgar/ver_pedido_adocao')
